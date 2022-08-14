3ca52992d17b5bd0361cc1f52cd945fae8e9d738
lmh12138 <mh.li298@foxmail.com>
2022-08-14 17:47:12 +0800

    rename the variable and class

---
+++ build/CMakeFiles/slam.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slam.dir/Algorithm/IMU/Fusion/FusionAhrs.c.o"
+  "CMakeFiles/slam.dir/Algorithm/IMU/Fusion/FusionCompass.c.o"
+  "CMakeFiles/slam.dir/Algorithm/IMU/Fusion/FusionOffset.c.o"
+  "CMakeFiles/slam.dir/Algorithm/IMU/ImuPoseSolve.cpp.o"
+  "CMakeFiles/slam.dir/Algorithm/VisualOdometer/FeaturePoints/MyFeaturePoints.cpp.o"
+  "CMakeFiles/slam.dir/Algorithm/VisualOdometer/MyVisualOdometer.cpp.o"
+  "CMakeFiles/slam.dir/Algorithm/distance_estimation.cpp.o"
+  "CMakeFiles/slam.dir/Algorithm/pose_estimation.cpp.o"
+  "CMakeFiles/slam.dir/Camera/RealSense/RealSense.cpp.o"
+  "CMakeFiles/slam.dir/MyThread.cpp.o"
+  "CMakeFiles/slam.dir/main.cpp.o"
+  "slam"
+  "slam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
